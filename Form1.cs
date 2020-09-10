15f74e604bfdce5affc2d541dd0824a252b2d2a5
satoshigp <satoshi3104>
2020-09-10 10:05:30 +0900

    テスト後　解答

--- Form1.cs
+++ Form1.cs
@@ -51,28 +51,28 @@ namespace v0910
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            for (int i = 0; i < 3; i++)
+            for(int i=0;i<3;i++)
             {
+                labels[i].Left += vx[i];
+                labels[i].Top += vy[i];
 
-
-                vx[i] = rand.Next(-15, 16);
-                vy[i] = rand.Next(-15, 16);
-
-
-
-
-                labels[i] = new Label();
-                labels[i].AutoSize = true;
-                labels[i].Text = "★";
-                labels[i].Left = rand.Next(ClientSize.Width - labels[i].Width);
-                labels[i].Top = rand.Next(ClientSize.Height - labels[i].Height);
-
-                Controls.Add(labels[i]);
-
-                labels[0].Left +=  vx;
-                labels[0].Top += vy;
+                if(labels[i].Left<0)
+                {
+                    vx[i] = Math.Abs(vx[i]);
+                }
+                if (labels[i].Left < 0)
+                {
+                    vy[i] = Math.Abs(vy[i]);
+                }
+                    if (labels[i].Right > ClientSize.Width)
+                {
+                    vx[i] = Math.Abs(vx[i]);
+                }
+                     if(labels[i].Bottom > ClientSize.Height)
+                {
+                    vx[i] = Math.Abs(vx[i]);
+                }
             }
-           
            
 
         }
